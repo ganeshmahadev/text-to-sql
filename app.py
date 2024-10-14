1df0df936e6fb7455a4cc9f5e6a9c7813d69915e
ganeshmahadev <ganeshmahadev2463@gmail.com>
2024-10-14 13:52:47 +0200

    commit with sql query and output

--- app.py
+++ app.py
@@ -10,18 +10,20 @@ import google.generativeai as genai
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-#function to load gemini model and provide sql query as response
+# Function to load gemini model and provide SQL query as response
 def get_gemini_pro(question, prompt):
-    model=genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(prompt + question)
+    # Convert prompt list to a single string
+    prompt_str = ''.join(prompt)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt_str + question)
     return response.text
 
-#function to retrive the query from sql database
-def read_sql_query(sql,db):
-    conn=sqlite3.connect(db)
-    curr=conn.cursor()
+# Function to retrieve the query from the SQL database
+def read_sql_query(sql, db):
+    conn = sqlite3.connect(db)
+    curr = conn.cursor()
     curr.execute(sql)
-    rows=curr.fetchall()
+    rows = curr.fetchall()
     conn.commit()
     conn.close()
     for row in rows:
@@ -29,7 +31,7 @@ def read_sql_query(sql,db):
     return rows
 
 ## Define Your Prompt
-prompt=[
+prompt = [
     """
     You are an expert in converting English questions to SQL query!
     The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
@@ -38,11 +40,8 @@ prompt=[
     \nExample 2 - Tell me all the students studying in Data Science class?, 
     the SQL command will be something like this SELECT * FROM STUDENT 
     where CLASS="Data Science"; 
-    also the sql code should not have ``` in beginning or end and sql word in output
-
+    also the SQL code should not have ``` in beginning or end and sql word in output.
     """
-
-
 ]
 
 ## Streamlit App
@@ -50,19 +49,22 @@ prompt=[
 st.set_page_config(page_title="I can Retrieve Any SQL query")
 st.header("Gemini App To Retrieve SQL Data")
 
-question=st.text_input("Input: ",key="input")
+question = st.text_input("Input: ", key="input")
 
-submit=st.button("Ask the question")
+submit = st.button("Ask the question")
 
-# if submit is clicked
+# If submit is clicked
 if submit:
-    response=get_gemini_pro(question,prompt)
-    print(response)
-    response=read_sql_query(response,"student.db")
-    st.subheader("The REsponse is")
+    # Get the generated SQL query
+    response = get_gemini_pro(question, prompt)
+    
+    # Display the SQL query in the Streamlit app
+    st.subheader("Generated SQL Query:")
+    st.code(response, language="sql")
+    
+    # Execute the SQL query and show the results
+    response = read_sql_query(response, "student.db")
+    
+    st.subheader("Query Results:")
     for row in response:
-        print(row)
-        st.header(row)
-
-
-
+        st.write(row)
